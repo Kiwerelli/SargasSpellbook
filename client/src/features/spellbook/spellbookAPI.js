95e6e0d7bf0ea4300dcba431a0a6f897d66ae4df
Kiwerelli <kevin.baldassari@gmail.com>
2022-10-30 23:13:05 +0100

    new dependencies

--- client/src/features/spellbook/spellbookAPI.js
+++ client/src/features/spellbook/spellbookAPI.js
@@ -1,6 +1,6 @@
 // A mock function to mimic making an async request for data
 export function fetchAll() {
-    const url = 'http://localhost:5000/spellbook';
+    const url = 'http://129.152.24.79/spellbook';
     return fetch(url).then((response) => {
         return response.json()
     })
